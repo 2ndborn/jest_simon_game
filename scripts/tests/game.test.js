30a7815679798d272bb04627a1e0e149d965c3ad
2ndborn <>
2023-09-15 20:42:40 +0000

    final challenge

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -33,6 +33,15 @@ describe("game object contains correct keys", () => {
     test("turnNumber key exists", () => {
         expect("turnNumber" in game).toBe(true);
     });
+    test("lastButton keys exist", () => {
+        expect("lastButton" in game).toBe(true);
+    });
+    test("turnInProgress keys exist", () => {
+        expect("turnInProgress" in game).toBe(true);
+    });
+    test("turnInProgress key value is false", () => {
+        expect("turnInProgress" in game).toBe(true);
+    });
 });
 
 describe("newGame works correctly", () => {
@@ -102,10 +111,20 @@ describe("gameplay works correctly", () => {
         game.playerMoves.push(game.currentGame[0]);
         playerTurn();
         expect(game.score).toBe(1);
-    })
+    });
     test("should call an alert if the move is wrong", () => {
         game.playerMoves.push("Wrong");
         playerTurn();
         expect(window.alert).toBeCalledWith("Wrong move!");
-    })
+    });
+    test("should toggle turnInProgress to be true", () => {
+          showTurns();
+          expect(game.turnInProgress).toBe(true);  
+    });
+    test("clicking during computer sequence should fail", () => {
+        showTurns();
+        game.lastButton = "";
+        document.getElementById("button2").click();
+        expect(game.lastButton).toEqual("");
+    });
 });
